a009a1ca33dd0dd77f7aedf9d1f239a92e96da19
Y RAVI TEJA <yraviteja44@gmail.com>
2026-09-24 00:52:34 +0530

    Time: 0 ms (100%), Space: 47.7 MB (53.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,33 @@
 class Solution {
-    public int trap(int[] arr) {
-        int[] lb = new int[arr.length];
-        int[] rb = new int[arr.length];
-        lb[0]= arr[0];
-        for(int i =1;i<arr.length;i++){
-            if(arr[i]>lb[i-1]){
-                lb[i] = arr[i];
-            }
-            else{
-                lb[i] = lb[i-1];
-            }
-        }
-        rb[arr.length-1] =arr[arr.length-1];
-        for(int i= arr.length-2;i>=0;i--){
-            if(arr[i]>rb[i+1]){
-                rb[i] = arr[i];
-            }else{
-                rb[i] = rb[i+1];
-            }
-        }
+    public int trap(int[] height) {
+        int lhb = height[0];
+        int rhb = height[height.length - 1];
+        int l =0;
+        int r = height.length-1;
         int res =0;
-        for(int i = 1 ;i<arr.length-1;i++){
-            int x = Math.min(lb[i],rb[i]);
-            res += x - arr[i];
+        while(l<=r){
+            if(lhb<=rhb){
+                if(height[l]>=lhb){
+                    lhb = height[l];
+                }else{
+                    res = res+lhb -height[l];
+                    
+                }
+                l++;
+
+
+            }else{
+                if(height[r]>=rhb){
+                    rhb = height[r];
+                }else{
+                    res = res +rhb-height[r];
+                }
+                r--;
+
+            }
+
         }
-        
         return res;
-
-
 
 
     }
